1fd5d7c474ebcac94fd33cac78f39363e05a7ba5
Talha-G <mtalhagujjar285@gmail.com>
2023-09-14 17:33:53 +0500

    Completed Successfully

---
+++ src/Ap.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import "./Ap.css";
+
+function Ap() {
+  
+ 
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+  
+ 
+  return (
+    <>
+    <div className="container">
+      <div className="list">
+
+        <div className="head">
+           <h2>To Do-List</h2>
+           <img src="../img/icon.png" alt="logo" />
+        </div>
+
+          <div className="row">
+           <input type="text" id='input-box' placeholder='Add your text'
+            value={task}
+           onChange={(e) => setTask(e.target.value)}/>
+
+            <button onClick={addTask}>Add</button>
+          </div>
+            
+            <ul id ='list-task'>
+             {todos.map((todo, index) => (
+             <li key={index}>
+               {todo}
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+          
+     </div>
+     
+     </>
+  )
+}
+
+export default Ap;
